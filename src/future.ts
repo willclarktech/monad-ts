6612d4c1f2d8493a19f6ccb06522911e488f96e9
willclarktech <willclarktech@users.noreply.github.com>
2023-09-30 17:42:51 +0100

    Rewrite Future for factory approach

--- src/future.ts
+++ src/future.ts
@@ -1,4 +1,11 @@
-import type { Monad } from "./monad";
+import type { IMonad } from "./monad";
+import type { Writable } from "./util";
+
+type Resolve<A> = (value: A) => void;
+
+type Reject = (error: Error) => void;
+
+type Executor<A> = (resolve: Resolve<A>, reject: Reject) => void;
 
 enum FutureState {
 	Pending,
@@ -6,8 +13,12 @@ enum FutureState {
 	Rejected,
 }
 
-interface Pending {
+interface Pending<A> {
 	readonly state: FutureState.Pending;
+	// eslint-disable-next-line functional/prefer-readonly-type
+	readonly onFulfilledCallbacks: Resolve<A>[];
+	// eslint-disable-next-line functional/prefer-readonly-type
+	readonly onRejectedCallbacks: Reject[];
 }
 
 interface Fulfilled<A> {
@@ -20,155 +31,213 @@ interface Rejected {
 	readonly error: Error;
 }
 
-type FutureData<A> = Pending | Fulfilled<A> | Rejected;
+type Future<A> = Pending<A> | Fulfilled<A> | Rejected;
 
-type Resolve<A> = (value: A) => void;
+export interface IFuture<A, B>
+	extends IMonad<
+		A,
+		Future<A>,
+		Future<Future<A>>,
+		B,
+		Future<B>,
+		Future<(a: A) => B>
+	> {
+	readonly pending: <C>() => Future<C>;
+	readonly reject: <C>(error: Error, f?: Future<C>) => Future<C>;
+	readonly resolve: <C>(value: C, f?: Future<C>) => Future<C>;
+	readonly isPending: <C>(f: Future<C>) => f is Pending<C>;
+	readonly isFulfilled: <C>(f: Future<C>) => f is Fulfilled<C>;
+	readonly isRejected: <C>(f: Future<C>) => f is Rejected;
+	readonly new: <C>(e: Executor<C>) => Future<C>;
+	readonly then: <C>(
+		resolve: (a: A) => C,
+		f: Future<A>,
+		reject?: (error: Error) => C,
+	) => Future<C>;
+	readonly catch: <C>(reject: (error: Error) => C, f: Future<A>) => Future<C>;
+	readonly finally: (handler: () => void, f: Future<A>) => Future<A>;
+	// Functor
+	readonly fmap: <C>(fab: (a: A) => C, m: Future<A>) => Future<C>;
+	// Applicative
+	readonly pure: <C>(a: C) => Future<C>;
+	readonly apply: (fab: Future<(a: A) => B>, m: Future<A>) => Future<B>;
+	// Monad
+	readonly join: <C>(mma: Future<Future<C>>) => Future<C>;
+	readonly bind: (m: Future<A>, fab: (a: A) => Future<B>) => Future<B>;
+}
 
-type Reject = (error: Error) => void;
+type FutureFactory = { new <A, B>(): IFuture<A, B> };
 
-type Executor<A> = (resolve: Resolve<A>, reject: Reject) => void;
-
-export class Future<A> implements Monad<A> {
-	// eslint-disable-next-line functional/prefer-readonly-type
-	private readonly onFulfilledCallbacks: Resolve<A>[] = [];
-	// eslint-disable-next-line functional/prefer-readonly-type
-	private readonly onRejectedCallbacks: Reject[] = [];
-
-	// eslint-disable-next-line functional/prefer-readonly-type
-	private constructor(public data: FutureData<A>) {}
-
-	private static pending<B>(): Future<B> {
-		return new Future({ state: FutureState.Pending });
-	}
-
-	public static reject<B>(error: Error): Future<B> {
-		return new Future({ state: FutureState.Rejected, error });
-	}
-
-	public static resolve<B>(value: B): Future<B> {
-		return new Future({ state: FutureState.Fulfilled, value });
-	}
-
-	public isPending(): this is { readonly data: Pending } {
-		return this.data.state === FutureState.Pending;
-	}
-
-	public isFulfilled(): this is { readonly data: Fulfilled<A> } {
-		return this.data.state === FutureState.Fulfilled;
-	}
-
-	public isRejected(): this is { readonly data: Rejected } {
-		return this.data.state === FutureState.Rejected;
-	}
-
-	private reject(error: Error): Future<A> {
-		if (this.data.state === FutureState.Pending) {
-			this.data = {
-				state: FutureState.Rejected,
-				error,
-			};
-			this.onRejectedCallbacks.forEach((cb) => cb(error));
+export const Future: FutureFactory = function <A, B>(
+	this: Writable<IFuture<A, B>>,
+): void {
+	this.pending = <C>(): Future<C> => ({
+		state: FutureState.Pending,
+		onFulfilledCallbacks: [],
+		onRejectedCallbacks: [],
+	});
+	this.reject = <C>(error: Error, f?: Future<C>): Future<C> => {
+		if (f !== undefined) {
+			if (f.state !== FutureState.Pending) {
+				return f;
+			}
+			f.onRejectedCallbacks.forEach((cb) => cb(error));
 		}
-		return this;
-	}
-
-	private resolve(value: A): Future<A> {
-		if (this.data.state === FutureState.Pending) {
-			this.data = {
-				state: FutureState.Fulfilled,
-				value,
-			};
-			this.onFulfilledCallbacks.forEach((cb) => cb(value));
+		return {
+			state: FutureState.Rejected,
+			error,
+		};
+	};
+	this.resolve = <C>(value: C, f?: Future<C>): Future<C> => {
+		if (f !== undefined) {
+			if (f.state !== FutureState.Pending) {
+				return f;
+			}
+			f.onFulfilledCallbacks.forEach((cb) => cb(value));
 		}
-		return this;
-	}
-
-	public static new<B>(f: Executor<B>): Future<B> {
-		const future = Future.pending<B>();
+		return {
+			state: FutureState.Fulfilled,
+			value,
+		};
+	};
+	this.isPending = <C>(f: Future<C>): f is Pending<C> =>
+		f.state === FutureState.Pending;
+	this.isFulfilled = <C>(f: Future<C>): f is Fulfilled<C> =>
+		f.state === FutureState.Fulfilled;
+	this.isRejected = <C>(f: Future<C>): f is Rejected =>
+		f.state === FutureState.Rejected;
+	this.new = <C>(e: Executor<C>): Future<C> => {
+		const f = this.pending<C>();
 		try {
-			f(future.resolve.bind(future), future.reject.bind(future));
-			return future;
+			e(
+				(value) => this.resolve(value, f),
+				(error) => this.reject(error, f),
+			);
+			return f;
 		} catch (error) {
 			const errorToThrow =
 				error instanceof Error ? error : new Error("Unrecognized error");
-			return Future.reject(errorToThrow);
+			return this.reject(errorToThrow, f);
 		}
-	}
-
-	public then<B>(f: (a: A) => B, g?: (error: Error) => B): Future<B> {
-		const future = Future.pending<B>();
-		this.onFulfilledCallbacks.push((a) => future.resolve(f(a)));
-		if (g !== undefined) {
-			this.onRejectedCallbacks.push((error) => future.resolve(g(error)));
+	};
+	this.then = <C, D>(
+		resolve: (a: A) => C,
+		f: Future<A>,
+		reject?: (error: Error) => D,
+	): Future<typeof reject extends undefined ? C : C | D> => {
+		const g = this.pending<C | D>();
+		switch (f.state) {
+			case FutureState.Pending:
+				// eslint-disable-next-line functional/immutable-data
+				f.onFulfilledCallbacks.push((value) => this.resolve(resolve(value), g));
+				if (reject !== undefined) {
+					// eslint-disable-next-line functional/immutable-data
+					f.onRejectedCallbacks.push((error) => this.resolve(reject(error), g));
+				}
+				break;
+			case FutureState.Fulfilled:
+				this.resolve(resolve(f.value), g);
+				break;
+			case FutureState.Rejected:
+				if (reject !== undefined) {
+					this.resolve(reject(f.error), g);
+				}
+				break;
 		}
-		return future;
-	}
-
-	public catch<B>(f: (error: Error) => B): Future<B> {
-		const future = Future.pending<B>();
-		this.onRejectedCallbacks.push((error) => future.resolve(f(error)));
-		return future;
-	}
-
-	public finally(f: () => void): Future<A> {
-		const future = Future.pending<A>();
-		const fWrapped = () => {
+		return g;
+	};
+	this.catch = <C>(reject: (error: Error) => C, f: Future<A>): Future<C> => {
+		const g = this.pending<C>();
+		switch (f.state) {
+			case FutureState.Pending:
+				// eslint-disable-next-line functional/immutable-data
+				f.onRejectedCallbacks.push((error) => this.resolve(reject(error), g));
+				break;
+			case FutureState.Fulfilled:
+				break;
+			case FutureState.Rejected:
+				this.resolve(reject(f.error), g);
+				break;
+		}
+		return g;
+	};
+	this.finally = (handler: () => void, f: Future<A>): Future<A> => {
+		const g = this.pending<A>();
+		const handlerWrapped = () => {
 			try {
-				f();
+				handler();
 			} catch {
 				// Do nothing
 			}
 		};
-		this.onFulfilledCallbacks.push((a) => {
-			fWrapped();
-			future.resolve(a);
-		});
-		this.onRejectedCallbacks.push((error) => {
-			fWrapped();
-			future.reject(error);
-		});
-		return future;
-	}
-
-	public static empty = Future.pending.bind(Future);
-
-	public fmap = this.then.bind(this);
-
-	public static pure = Future.resolve.bind(Future);
-
-	public apply<B>(fab: Future<(a: A) => B>): Future<B> {
-		const future = Future.pending<B>();
-
-		this.then((a) => {
-			fab.then((f) => future.resolve(f(a)));
-		});
-
-		return future;
-	}
-
-	public static join<B>(future: Future<Future<B>>): Future<B> {
-		const flattened = Future.pending<B>();
-
-		future.then(
+		const onFulfilled = (v: A): void => {
+			handlerWrapped();
+			this.resolve(v, g);
+		};
+		const onRejected = (e: Error): void => {
+			handlerWrapped();
+			this.reject(e, g);
+		};
+		switch (f.state) {
+			case FutureState.Pending:
+				// eslint-disable-next-line functional/immutable-data
+				f.onFulfilledCallbacks.push((a) => {
+					onFulfilled(a);
+				});
+				// eslint-disable-next-line functional/immutable-data
+				f.onRejectedCallbacks.push((error) => {
+					onRejected(error);
+				});
+				break;
+			case FutureState.Fulfilled:
+				onFulfilled(f.value);
+				break;
+			case FutureState.Rejected:
+				onRejected(f.error);
+				break;
+		}
+		return g;
+	};
+	// Functor
+	this.fmap = this.then;
+	// // Applicative
+	this.pure = this.resolve;
+	this.apply = <C>(fab: Future<(a: A) => C>, f: Future<A>): Future<C> => {
+		const g = this.pending<C>();
+		this.then(
+			(a) =>
+				new Future<(a: A) => C, unknown>().then(
+					(fun) => this.resolve(fun(a), g),
+					fab,
+				),
+			f,
+		);
+		return g;
+	};
+	this.join = <C>(f: Future<Future<C>>): Future<C> => {
+		const flattened = this.pending<C>();
+		new Future<Future<C>, unknown>().then(
 			(innerFuture) => {
-				innerFuture.then(
+				new Future<C, A>().then(
 					(value) => {
-						flattened.resolve(value);
+						this.resolve(value, flattened);
 					},
+					innerFuture,
 					(error) => {
-						flattened.reject(error);
+						this.reject(error, flattened);
 					},
 				);
 			},
+			f,
 			(error) => {
-				flattened.reject(error);
+				this.reject(error, flattened);
 			},
 		);
-
 		return flattened;
-	}
+	};
+	this.bind = (m: Future<A>, fab: (a: A) => Future<B>): Future<B> =>
+		this.join(this.fmap(fab, m));
 
-	public bind<B>(f: (a: A) => Future<B>): Future<B> {
-		return Future.join(this.fmap(f));
-	}
-}
+	// NOTE: TS apparently has trouble typing constructible functions
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any as FutureFactory;
