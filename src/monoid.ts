d60eee886e076fcdbb441f6547f2a4989db9c58e
willclarktech <willclarktech@users.noreply.github.com>
2023-09-30 17:42:13 +0100

    Rewrite Monoid types for factory approach

--- src/monoid.ts
+++ src/monoid.ts
@@ -1,8 +1,15 @@
-export interface Semigroup<A> {
-	concat(semigroup: Semigroup<A>): Semigroup<A>;
+// NOTE: Only here to indicate that Semigroups should be parameterized by one type
+// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unused-vars
+export interface Semigroup<A> {}
+
+export interface ISemigroup<A, S extends Semigroup<A>> {
+	readonly concat: (s1: S, s2: S) => S;
 }
 
+// NOTE: Only here to indicate that Monoids should be parameterized by one type
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Monoid<A> extends Semigroup<A> {
-	// empty should be a static method on the class
+export interface Monoid<A> extends Semigroup<A> {}
+
+export interface IMonoid<A, M extends Monoid<A>> extends ISemigroup<A, M> {
+	readonly empty: () => M;
 }
