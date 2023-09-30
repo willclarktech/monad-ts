0ce7001016bfd4ee66590b1e33e6b75007699ba8
willclarktech <willclarktech@users.noreply.github.com>
2023-09-30 17:43:10 +0100

    Rewrite Writer for factory approach

--- src/writer.ts
+++ src/writer.ts
@@ -1,40 +1,79 @@
-import type { Monad } from "./monad";
-import type { Monoid } from "./monoid";
+import type { IMonad } from "./monad";
+import type { IMonoid, Monoid } from "./monoid";
+import type { Writable } from "./util";
 
-export const writerFactory: <B, M extends Monoid<B>>(
-	emptyMonoid: M,
-) => { new (value: B, logs: M): Monad<B>; pure<A>(value: A): Monad<A> } = <
-	B,
-	M extends Monoid<B>,
->(
-	emptyMonoid: M,
-) =>
-	class Writer<A> implements Monad<A> {
-		public constructor(public readonly value: A, public readonly logs: M) {}
+export type Writer<A, T, W extends Monoid<T>> = {
+	readonly value: A;
+	readonly logs: W;
+};
 
-		public fmap<C>(f: (a: A) => C): Writer<C> {
-			return new Writer(f(this.value), this.logs);
-		}
+export interface IWriter<A, B, T, W extends Monoid<T>>
+	extends IMonad<
+		A,
+		Writer<A, T, W>,
+		Writer<Writer<A, T, W>, T, W>,
+		B,
+		Writer<B, T, W>,
+		Writer<(a: A) => B, T, W>
+	> {
+	// Functor
+	readonly fmap: <C>(fab: (a: A) => C, w: Writer<A, T, W>) => Writer<C, T, W>;
+	// Applicative
+	readonly pure: <C>(a: C) => Writer<C, T, W>;
+	readonly apply: (
+		fab: Writer<(a: A) => B, T, W>,
+		w: Writer<A, T, W>,
+	) => Writer<B, T, W>;
+	// Monad
+	readonly join: <C>(wwa: Writer<Writer<C, T, W>, T, W>) => Writer<C, T, W>;
+	readonly bind: (
+		w: Writer<A, T, W>,
+		fab: (a: A) => Writer<B, T, W>,
+	) => Writer<B, T, W>;
+}
 
-		public static pure<C>(value: C): Writer<C> {
-			return new Writer(value, emptyMonoid);
-		}
+type WriterFactory = {
+	new <A, B, T, W extends Monoid<T>>(monoid: IMonoid<A, W>): IWriter<
+		A,
+		B,
+		T,
+		W
+	>;
+};
 
-		public apply<C>(fac: Writer<(a: A) => C>): Writer<C> {
-			const newValue = fac.value(this.value);
-			// TODO: Figure out if it's possible to avoid this cast
-			const newLogs = this.logs.concat(fac.logs) as M;
-			return new Writer(newValue, newLogs);
-		}
+export const Writer: WriterFactory = function <A, B, T, W extends Monoid<T>>(
+	this: Writable<IWriter<A, B, T, W>>,
+	monoid: IMonoid<A, W>,
+): void {
+	// Functor
+	this.fmap = <C>(fab: (a: A) => C, w: Writer<A, T, W>): Writer<C, T, W> => ({
+		value: fab(w.value),
+		logs: w.logs,
+	});
 
-		public static join<C>(writer: Writer<Writer<C>>): Writer<C> {
-			const innerWriter = writer.value;
-			// TODO: Figure out if it's possible to avoid this cast
-			const newLogs = writer.logs.concat(innerWriter.logs) as M;
-			return new Writer(innerWriter.value, newLogs);
-		}
+	// Applicative
+	this.pure = <C>(c: C): Writer<C, T, W> => ({
+		value: c,
+		logs: monoid.empty(),
+	});
+	this.apply = (
+		fab: Writer<(a: A) => B, T, W>,
+		w: Writer<A, T, W>,
+	): Writer<B, T, W> => ({
+		value: fab.value(w.value),
+		logs: monoid.concat(w.logs, fab.logs),
+	});
 
-		public bind<C>(f: (a: A) => Writer<C>): Writer<C> {
-			return Writer.join(this.fmap(f));
-		}
-	};
+	// Monad
+	this.join = <C>(wwa: Writer<Writer<C, T, W>, T, W>): Writer<C, T, W> => ({
+		value: wwa.value.value,
+		logs: monoid.concat(wwa.logs, wwa.value.logs),
+	});
+	this.bind = (
+		w: Writer<A, T, W>,
+		fab: (a: A) => Writer<B, T, W>,
+	): Writer<B, T, W> => this.join(this.fmap(fab, w));
+
+	// NOTE: TS apparently has trouble typing constructible functions
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any as WriterFactory;
